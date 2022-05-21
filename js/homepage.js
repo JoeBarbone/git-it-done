e0794610414d507ed07c6993f6e2ca7afafe09da
Joseph Barbone <JoeBarbone@aol.com>
2022-05-21 17:41:58 -0400

    6.2.6 add error catching

--- js/homepage.js
+++ js/homepage.js
@@ -22,6 +22,13 @@ var formSubmitHandler = function(event) {
 
 
 var displayRepos = function(repos, searchTerm) {
+    // check if the api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "no repos found";
+        return;
+    }
+    
+    
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTermEl.textContent = searchTerm;
@@ -80,12 +87,21 @@ var getUserRepos = function(user) {
 
     // make request to the url
     fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-        displayRepos(data, user)
-    });
+        if (response.ok) {
+            response.json().then(function(data) {
+            displayRepos(data, user) });
+         } else {
+            alert("Error: GitHub User Not Found");
+        }
+    
 
-    });
+    })
 
+    .catch(function(error) {
+        // "catch" getting chained to the end of the "then" statement
+        alert("unale to connect to GitHub");
+    });
+    
     console.log("outside");   
 
 }
